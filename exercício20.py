0b55040dccd0e7202d305eefac0826424cefd388
ViniciusDamiani <vinidamiani03@hotmail.com>
2020-08-31 16:38:25 -0300

    feat: exercicio20

---
+++ exercício20.py
@@ -0,0 +1,6 @@
+#Variaveis
+conta = float
+massaL =  650                               
+
+conta = (massaL / 0.45)
+print("Temos 20kg de arroz e se quisermos saber qual a massa em libras, seria:", round (conta), "libras")
