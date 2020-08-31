28584de78821730d94dca69741bfb806f2155a1f
ViniciusDamiani <vinidamiani03@hotmail.com>
2020-08-31 16:34:26 -0300

    feat: exercicio7

---
+++ exercício7.py
@@ -0,0 +1,7 @@
+#Variaveis
+conta = float
+tempF = 65
+
+conta = 5.0 * (tempF - 32.0)/9.0 
+print("A temperatura nos Estados Unidos é 25 graus Fahrenheit. Já no Brasil a temperatura é", round(conta) ,"graus Celsius")
+
